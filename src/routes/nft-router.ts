453f92ca80ec838334c8a245077c0a786a9ad4d9
crashdev226 <midashand44@gmail.com>
2022-04-01 14:16:04 +0300

    feat: get NFTs by collection slug

--- src/routes/nft-router.ts
+++ src/routes/nft-router.ts
@@ -1,29 +1,36 @@
 import { Request, Response, Router } from "express";
 import axios from "axios";
+import { wait } from "@utils/wait";
+
+const API_URL = "https://testnets-api.opensea.io/api/v1";
 
 const router = Router();
-
-router.get("/", async (req: Request, res: Response) => {
-  const { owner } = req.query;
-
-  const url = `https://testnets-api.opensea.io/api/v1/assets?owner=${owner}`;
-
-  const { data } = await axios.get(url);
-
-  return res.status(200).json(data);
-});
 
 router.get(
   "/:contractAddress/:tokenId",
   async (req: Request, res: Response) => {
     const { contractAddress, tokenId } = req.params;
 
-    const url = `https://testnets-api.opensea.io/api/v1/asset/${contractAddress}/${tokenId}`;
+    const url = `${API_URL}/asset/${contractAddress}/${tokenId}`;
+
+    await wait(500);
 
     const { data } = await axios.get(url);
 
     return res.status(200).json(data);
   }
 );
+
+router.get("/", async (req: Request, res: Response) => {
+  const { collection } = req.query;
+
+  const url = `${API_URL}/assets?collection=${collection}`;
+
+  await wait(1000);
+
+  const { data } = await axios.get(url);
+
+  return res.status(200).json(data);
+});
 
 export default router;
